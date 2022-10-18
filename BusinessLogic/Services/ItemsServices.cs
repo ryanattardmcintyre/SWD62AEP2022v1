3866d3b5084a825c789b8dc17b1827c0e3b3b40a
Ryan Attard <ryanattard@gmail.com>
2022-10-18 13:55:37 +0200

    Completed BusinessLogic First Controller

--- BusinessLogic/Services/ItemsServices.cs
+++ BusinessLogic/Services/ItemsServices.cs
@@ -1,14 +1,57 @@
-﻿using System;
+﻿using BusinessLogic.ViewModels;
+using DataAccess.Repositories;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BusinessLogic.Services
 {
+    //Item.cs (Domain) - is used to model/shape/generate/engineer the database
+    //e.g. User.cs
+
+    //CreateItemViewModel.cs (BusinessLogic)
+
     public class ItemsServices
     {
-        public ItemsServices() { }
 
-        public void AddItem(string name )
-        { }
+        //Constructor Injection
+        //Dependency Injection is a design pattern which handles the creation of instances in a centralized location for better
+        //efficiency
+
+        private ItemsRepository ir;
+        public ItemsServices(ItemsRepository _itemRepository) {
+            ir = _itemRepository;
+        }
+
+        public void AddItem(CreateItemViewModel item)
+        {
+             
+            if (ir.GetItems().Any(myItem => myItem.Name == item.Name))
+                throw new Exception("Item with the same name already exists");
+            else
+            {
+                ir.AddItem(new Domain.Models.Item()
+                {
+                    CategoryId = item.CategoryId, //AutoMapper
+                    Description = item.Description,
+                    Name = item.Name,
+                    PhotoPath = item.PhotoPath,
+                    Price = item.Price,
+                    Stock = item.Stock
+                });
+            }
+        }
+
+        public void DeleteItem(int id)
+        {  
+        }
+
+        public void Checkout()
+        {
+             
+        }
+
+       
     }
 }
---
+++ WebApplication1/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLogic.Services;
+using BusinessLogic.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    //are going to handle the incoming requests and outgoing responses
+    public class ItemsController : Controller
+    {
+        private ItemsServices itemsService;
+        public ItemsController(ItemsServices _itemsService)
+        { itemsService = _itemsService; }
+
+        //a method to open the page, then the user starts typing
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //a method to handle the submission of the form
+        [HttpPost]
+        public IActionResult Create(CreateItemViewModel data)
+        { //.....
+            itemsService.AddItem(data); //to test
+
+            return View();
+        }
+    }
+}
